5a1b286fbab6309b6339cf2fc20590e73f7d2bb5
Bryan Leanoe <bryan.leano@gmail.com>
2018-10-16 22:22:17 -0600

    add GamePlaying state

--- app.js
+++ app.js
@@ -9,43 +9,44 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-
-  dice = Math.floor(Math.random() * 6) + 1;
-
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
-
-  if (dice !== 1) {
-    roundScore += dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
-  } 
-  
+  if(gamePlaying) {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+      if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+      } else {
+        nextPlayer();
+      }
+  }    
 });
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
- scores[activePlayer] += roundScore;
-
- document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-  if (scores[activePlayer] >= 20) {
-    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-    document.querySelector('.dice').style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  if (gamePlaying) {
+    scores[activePlayer] += roundScore;
     
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    if (scores[activePlayer] >= 100) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
   } else {
     nextPlayer();
   }
-  
+}
 });
 
 function nextPlayer() {
@@ -56,7 +57,7 @@ function nextPlayer() {
   document.getElementById('current-1').textContent = '0';
 
   document.querySelector('.player-0-panel').classList.toggle('active');
-  document.querySelector('.player-1-panel').classList.toggle('active');  
+  document.querySelector('.player-1-panel').classList.toggle('active');
 
   //document.querySelector('.player-0-panel').classList.remove('active');
   //document.querySelector('.player-1-panel').classList.add('active');  
@@ -70,9 +71,10 @@ function init() {
   scores = [0, 0];
   activePlayer = 0;
   roundScore = 0;
-
-  document.querySelector('.dice').style.display = 'none';
+  gamePlaying = true;
   
+  document.querySelector('.dice').style.display = 'none';
+
   document.getElementById('score-0').textContent = '0';
   document.getElementById('score-1').textContent = '0';
   document.getElementById('current-0').textContent = '0';
@@ -83,8 +85,7 @@ function init() {
   document.querySelector('.player-1-panel').classList.remove('winner');
   document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.remove('active');
-  document.querySelector('.player-0-panel').classList.add('active');  
-  
+  document.querySelector('.player-0-panel').classList.add('active');
 }
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
